94348d65247578db9213b31a6f4af020a1cace97
Huda Mawood <hudamawood@gmial.com>
2026-02-24 13:35:55 +0200

    are you okay

--- src/test.sh
+++ src/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-output=$(python app.py)
+output=$(python app.py | tr -d '\r\n')
 
 if [ "$output" == "Hello from Python! i hate you foreverrrrrr" ]; then
-    echo "Test Passed "
+    echo "Test Passed"
 else
-    echo "Test Failed "
+    echo "Test Failed"
 fi
