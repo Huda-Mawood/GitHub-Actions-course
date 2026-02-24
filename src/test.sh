9d1b90efa59cb68150769f0070a953c9352273bf
Huda Mawood <hudamawood@gmial.com>
2026-02-24 13:45:26 +0200

    are you okay

--- src/test.sh
+++ src/test.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-output=$(python app.py)
+# شغل app.py وخزن الناتج
+python app.py > tmp_output.txt
 
-if [[ "$output" == *"Hello from Python! i hate you foreverrrrrr"* ]]; then
+# ابحث عن النص المطلوب
+if grep -q "Hello from Python! i hate you foreverrrrrr" tmp_output.txt; then
     echo "Test Passed"
 else
     echo "Test Failed"
 fi
+
+# امسحي الملف المؤقت
+rm tmp_output.txt
