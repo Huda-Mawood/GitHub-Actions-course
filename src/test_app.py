efa32e0d9531532fade2925bda88801ef2f0a350
Huda Mawood <hudamawood@gmial.com>
2026-02-24 14:00:02 +0200

    are you okay

--- src/test_app.py
+++ src/test_app.py
@@ -3,13 +3,9 @@ from pathlib import Path
 import subprocess
 import sys
 
-# المسار الصحيح لـ app.py
 app_path = Path(__file__).parent / "app.py"
 
-# شغّل Python بنفس النسخة اللي شغّلة السكربت
 result = subprocess.run([sys.executable, str(app_path)], capture_output=True, text=True)
-
-# نظّف الناتج من أي newline أو carriage return
 output = result.stdout.strip()
 
 expected = "Hello from Python! i hate you foreverrrrrrr"
